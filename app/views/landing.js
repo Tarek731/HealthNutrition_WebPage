de66987cb87c3954e150931602c59421828ba388
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-09-09 16:22:36 -0400

    fat search script added to index.html page only

--- app/views/landing.js
+++ app/views/landing.js
@@ -9,7 +9,8 @@ var Landing = React.createClass({
     return (
 		<div>
 		<h1>Landing Page</h1>
-   			
+   			<h2>FatSecret!!!</h2>
+        <script src="http://platform.fatsecret.com/js?key=f1b3a606a7f04808a468ebcd2ab47b68&auto_load=true"></script>
  		</div>   
     );
   }
